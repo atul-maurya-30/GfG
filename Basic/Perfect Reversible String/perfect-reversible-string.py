8e06b31b20ff6dce7e6eee0ff97a19d04ae3a816
kashi-ff <119139143+kashi-ff@users.noreply.github.com>
2024-02-15 01:44:04 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Perfect Reversible String/perfect-reversible-string.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+    def isReversible(self, str, n):
+        # code here
+        if str==str[::-1]:
+            return 1
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input().strip())
+        str= input().strip()
+        ob = Solution()
+        print(ob.isReversible(str, n))
+# } Driver Code Ends
