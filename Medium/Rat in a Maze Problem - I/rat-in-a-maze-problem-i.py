eeda424e2d0d0a2ff03bf4fb508a07cc2b5cea36
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-04-20 18:34:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.py
+++ Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.py
@@ -2,34 +2,32 @@
 
 class Solution:
     def findPath(self, m, n):
-        directions = "DLRU"
-        dr = [1, 0, 0, -1]
-        dc = [0, -1, 1, 0]
-
-        def is_valid(row, col, n, maze):
-            return 0 <= row < n and 0 <= col < n and maze[row][col] == 1
-
-        def explore(row, col, maze, path, all_paths, m, n):
-            if row == m - 1 and col == n - 1:
-                all_paths.append(path)
-                return
-
-            maze[row][col] = 0
-
-            for i in range(len(directions)):
-                new_row, new_col = row + dr[i], col + dc[i]
-                if is_valid(new_row, new_col, m, maze):
-                    explore(new_row, new_col, maze, path + directions[i], all_paths, m, n)
-
-            maze[row][col] = 1
-
-        result = []
-        if m[0][0] == 1 and m[n - 1][n - 1] == 1:
-            explore(0, 0, m, '', result, len(m), len(m[0]))
-
+        #define the directions and changes in row and columns indices for each direction
+        directions="DLRU"
+        dr=[1,0,0,-1]
+        dc=[0,-1,1,0]
+        result=[] #list to store valid paths
+        
+        def is_valid(row,col,n,maze):
+            return 0<=row<n and 0<=col<n and maze[row][col]==1
+            
+        def explore(row,col,path,maze):
+            #IF REACHED the destination ,add the path to the result
+            if row==n-1 and col==n-1:
+                result.append(path)
+                return 
+            for i in range(4):
+                new_row,new_col=row+dr[i],col+dc[i]
+                #checks if the new cell is valid and unvisited
+                if is_valid(new_row,new_col,n,maze):
+                    maze[row][col]=0 #mark current cell as visited
+                    explore(new_row,new_col,path+directions[i],maze) #explore further paths
+                    maze[row][col]=1 #backtrack:restore current cell
+        #check if the start and end cells are unblocked 
+        if m[0][0]==1 and m[n-1][n-1]==1:
+            explore(0,0,"",m) #start exploring from the top-left corner
         return sorted(result)
-
-
+                
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
